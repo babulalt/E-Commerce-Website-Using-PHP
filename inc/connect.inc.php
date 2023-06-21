edb90aa6b0d3c7ec1c6c592a29749f67cb518b15
Babulal Tamang <84000496+babulalt@users.noreply.github.com>
2023-06-21 12:27:33 +0545

    Update connect.inc.php

--- inc/connect.inc.php
+++ inc/connect.inc.php
@@ -1,4 +1,4 @@
 <?php 
-	mysqli_connect("zerone-addon-8516-101-mariadb.zerone-4070-8516.svc.cluster.local","root","1wtjruZNz8") or die("Couldn't connet to SQL server");
-	mysqli_select_db("ecommerce") or die("Couldn'ttt select DB");
+	$link=mysqli_connect("zerone-addon-8516-101-mariadb.zerone-4070-8516.svc.cluster.local","root","1wtjruZNz8") or die("Couldn't connet to SQL server");
+	mysqli_select_db("ecommerce",$link) or die("Couldn'ttt select DB");
 ?>
